094155bba3747179cec86b81ab9e29d3203504f3
MuriloM <muriloommaia@gmail.com>
2022-01-18 14:16:50 -0300

    ajuste header e horarios

--- src/Components/DataInformacoes.jsx
+++ src/Components/DataInformacoes.jsx
@@ -8,25 +8,38 @@ class DataInformacoes extends React.Component {
         <hr className='hr50' />
         <div className='d-flex justify-content-evenly data-information align-items-center flex-wrap'>
           <div className='d-flex flex-column align-items-center justify-content-envenly'>
-            <p className='fw-bold'>Data de Início</p>
-            <p>Fevereiro 2022</p>
+            <p className='fw-bold'>Mês das aulas</p>
+            <p>Fevereiro</p>
           </div>
           <div className='d-flex flex-column align-items-center justify-content-envenly'>
-            <p className='fw-bold'>Horário das Aulas</p>
-            <p>Em breve mais informações</p>
+            <p className='fw-bold'>Dias de Aula</p>
+            <p>10, 11 e 12</p>
           </div>
           <div className='d-flex flex-column align-items-center justify-content-envenly'>
-            <p className='fw-bold'>Data de Finalização</p>
-            <p>Maio 2022</p>
+            <p className='fw-bold'>Horários de aula</p>
+            <p>Informações em breve</p>
           </div>
         </div>
-        <div className='d-flex flex-column align-items-center'>
-          <h4>Plataforma</h4>
-          <a href='https://zoom.us/download' target="_blank" rel="noreferrer">
-            <div className='icon-zoom px-3 py-2'>
-              <i className="fas fa-video"></i>
+        <div className="d-flex justify-content-around w-50">
+          <div className='d-flex flex-column align-items-center'>
+            <h4>Aulas no formato</h4>
+            <div className='d-flex px-3 pt-1'>
+              <i class="fas fa-user-friends"></i>
+              <p className="px-2">Presencial</p>
             </div>
-          </a>
+            <div className='d-flex px-3'>
+              <i class="fas fa-desktop"></i>
+              <p className="px-2">Virtual</p>
+            </div>
+          </div>
+          <div className='d-flex flex-column align-items-center'>
+            <h4>Plataforma Virtual</h4>
+            <a href='https://zoom.us/download' target="_blank" rel="noreferrer">
+              <div className='icon-zoom px-3 py-2 mt-2'>
+                <i className="fas fa-video"></i>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     )
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,9 @@ class Header extends React.Component {
       <div className="d-flex align-items-center p-3 bg-image">
         <div className='box-header'></div>
         <div className="header">
+          <h2>Organizar o</h2>
           <h1>Conselho Missionário</h1>
+          <h2>Da sua igreja Local</h2>
           <p>Pr. Djalma Albuquerque</p>
         </div>
       </div>
