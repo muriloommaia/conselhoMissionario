52d0fdd651dd9e292e8939b1c9807e6a4f2db0a4
MuriloM <muriloommaia@gmail.com>
2022-01-18 15:43:34 -0300

    cronograma de aulas

--- src/Components/Aulas.jsx
+++ src/Components/Aulas.jsx
@@ -25,9 +25,11 @@ class Aulas extends React.Component {
     const aulas = ['Aula 1 e 2', 'Aulas 3 a 5', 'Aula 6', 'Aula 7', 'Aula 8', 'Aula 9', 'Aula 10'];
     const classes = [<Aula1e2 />, <Aula345 />, <AulaSeis />, <AulaSete />, <AulaOito />, <AulaNove />, <AulaDez />];
     return (
-      <section className='bg-light p-4 text-center'>
+      <section className='bg-light p-2 pt-4 text-center'>
         {/* <hr className='hr50'/> */}
         <h1>Programa de Aulas</h1>
+        <p><span className="fw-bold">Dia 1</span> - Aulas 1 e 2  |  <span className="fw-bold">Dia 2</span> - Aulas 3 e 4</p>
+        <p><span className="fw-bold">Dia 3</span> - <span className="fw-bold">Manhã</span> - Aulas 4 à 7 | <span className="fw-bold">Tarde</span> - Aulas 8 à 10</p>
         {aulas.map((item, idx) => {
           return (
             <button
--- src/Components/DataInformacoes.jsx
+++ src/Components/DataInformacoes.jsx
@@ -4,6 +4,8 @@ class DataInformacoes extends React.Component {
   render() {
     return (
       <div className='d-flex flex-column align-items-center justify-content-evenly'>
+      <div className="d-flex w-100 bg-light flex-column align-items-center justify-content-evenly pt-3">
+        
         <h3>Informações Importantes</h3>
         <hr className='hr50' />
         <div className='d-flex justify-content-center data-information align-items-center flex-wrap'>
@@ -28,9 +30,10 @@ class DataInformacoes extends React.Component {
           </div>
         </div>
           <p className="diminute pt-2">* Horários com referência à Campo Grande - MS</p>
+      </div>
         <div className="d-flex justify-content-around w-50 pt-3">
           <div className='d-flex flex-column align-items-center'>
-            <h4>Aulas no formato</h4>
+            <h4 className="text-center">Aulas no formato</h4>
             <div className='d-flex px-3 pt-1'>
               <i class="fas fa-user-friends"></i>
               <p className="px-2">Presencial</p>
@@ -41,7 +44,7 @@ class DataInformacoes extends React.Component {
             </div>
           </div>
           <div className='d-flex flex-column align-items-center'>
-            <h4>Plataforma Virtual</h4>
+            <h4 className="text-center">Plataforma Virtual</h4>
             <a href='https://zoom.us/download' target="_blank" rel="noreferrer">
               <div className='icon-zoom px-3 py-2 mt-2'>
                 <i className="fas fa-video"></i>
